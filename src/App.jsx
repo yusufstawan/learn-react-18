4a82d3428bfff1f442f2f86670644451743cdbb0
yusufstawan <yusufsetiyawan45@gmail.com>
2023-10-06 10:24:50 +0700

    feat-component&props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+const Button = (props) => {
+  const { children = "...", variant = "bg-black" } = props;
+  return (
+    <button className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`} type="submit">
+      {children}
+    </button>
+  );
+};
+
 function App() {
   return (
-    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-      <div>Hello World</div>
+    <div className="flex justify-center bg-blue-300 min-h-screen items-center">
+      <div className="flex gap-x-5">
+        <Button variant="bg-red-700">Login</Button>
+        <Button variant="bg-slate-700">Logout</Button>
+        <Button variant="bg-black">Register</Button>
+        <Button></Button>
+      </div>
     </div>
   );
 }
