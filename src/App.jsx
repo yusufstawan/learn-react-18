7a3572cbb7d5df371f79246a4e3d65623b6682e9
yusufstawan <yusufsetiyawan45@gmail.com>
2023-10-06 09:11:50 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+      <div>Hello World</div>
+    </div>
+  );
+}
+
+export default App;
